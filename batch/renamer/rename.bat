0a40b280ee578d7494ad9c671a3c092ffe04f141
SannyQ <91934182+SannyQ@users.noreply.github.com>
2023-06-14 16:20:40 +0200

    Update rename.bat

--- batch/renamer/rename.bat
+++ batch/renamer/rename.bat
@@ -1,6 +1,14 @@
 @echo off
+setlocal
+set "folder=%cd%"
 
-for /r "%cd%" %%g in (*.txt) do ren "%%g" "%%~ng.php"
+echo Achtung! Alle .txt-Dateien in %folder% (und allen Unterordnern) werden in .php umbenannt.
+echo Drücken Sie eine beliebige Taste, um fortzufahren, oder schließen Sie dieses Fenster, um abzubrechen.
+pause >nul
 
-pause
+for /r "%folder%" %%g in (*.txt) do ren "%%g" "%%~ng.php"
+
+echo Umbenennen abgeschlossen! Drücken Sie eine beliebige Taste, um dieses Fenster zu schließen.
+pause >nul
+endlocal
 exit
