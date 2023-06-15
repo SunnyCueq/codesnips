593289ad4c425be264f1cc4065de31f1803bf594
SannyQ <91934182+SannyQ@users.noreply.github.com>
2023-06-15 18:29:17 +0200

    Update linkrypt.php

--- php/linkcrypter/FileCrypt.cc (Crypt Link + Status)/linkrypt.php
+++ php/linkcrypter/FileCrypt.cc (Crypt Link + Status)/linkrypt.php
@@ -1,8 +1,13 @@
+<?php
+
 function xml_to_array($xml, $main_heading = '') 
 { 
     $deXml     = simplexml_load_string($xml); 
     $deJson    = json_encode($deXml); 
     $xml_array = json_decode($deJson, true); 
+    if ($xml_array === null && json_last_error() !== JSON_ERROR_NONE) {
+        return 'JSON decoding error: ' . json_last_error_msg();
+    }
     if (!empty($main_heading)) { 
         $returned = $xml_array[$main_heading]; 
         return $returned; 
@@ -11,7 +16,6 @@ function xml_to_array($xml, $main_heading = '')
     } 
 } 
 
-// Linkcrypt.cc 
 function fileCrypt($downloadlinks, $filecrypt_api, $foldername) 
 { 
     $postdata = array( 
@@ -28,13 +32,18 @@ function fileCrypt($downloadlinks, $filecrypt_api, $foldername)
     $curl = curl_init(); 
     curl_setopt($curl, CURLOPT_URL, "https://filecrypt.cc/api.php"); 
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $postdata); //Setting post data as xml 
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $postdata); 
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); 
     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0); 
     $filecrypt_links = curl_exec($curl); 
+    if($filecrypt_links === false) {
+        return 'Curl error: ' . curl_error($curl);
+    }
     curl_close($curl); 
-    $xml        = $filecrypt_links; 
-    $data_array = xml_to_array($xml); 
+    $data_array = xml_to_array($filecrypt_links); 
+    if(is_string($data_array)) {
+        return $data_array; // Wenn xml_to_array einen Fehler zurückgibt
+    }
     $fc_link    = $data_array['container']['link']; 
     $fc_simg    = $data_array['container']['bigimg'] . ".png"; 
     $fc_array   = array( 
@@ -42,4 +51,12 @@ function fileCrypt($downloadlinks, $filecrypt_api, $foldername)
         $fc_simg 
     ); 
     return ($fc_array); 
-}  
+}
+
+$filecrypt_api = "IhrAPIKey"; // Setzen Sie hier Ihren Filecrypt-API-Schlüssel ein
+$downloadlinks = "IhreDownloadLinks"; // Setzen Sie hier Ihre Download-Links ein
+$foldername = "IhrOrdnername"; // Setzen Sie hier Ihren Ordnernamen ein
+
+$result = fileCrypt($downloadlinks, $filecrypt_api, $foldername);
+
+print_r($result); // Gibt das Array mit den Filecrypt-Links oder eine Fehlermeldung aus
