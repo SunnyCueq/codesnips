52603889fc6da5e903c025e76f84a19c81d22372
SannyQ <91934182+SannyQ@users.noreply.github.com>
2023-06-15 16:04:15 +0200

    Update rename.bat

--- batch/renamer/rename.bat
+++ batch/renamer/rename.bat
@@ -1,12 +1,32 @@
 @echo off
-setlocal
-set "folder=%cd%"
+setlocal enabledelayedexpansion
 
-echo Achtung! Alle .txt-Dateien in %folder% (und allen Unterordnern) werden in .php umbenannt.
+echo Geben Sie den Pfad zum Verzeichnis ein, in dem sich die Dateien befinden:
+set /p "sourceFolder="
+
+echo Geben Sie die Quell-Dateiendung ein, die umbenannt werden soll:
+set /p "sourceExtension="
+
+echo Geben Sie die Ziel-Dateiendung ein, in die umbenannt werden soll:
+set /p "targetExtension="
+
+echo Achtung! Alle Dateien mit der Endung %sourceExtension% im Verzeichnis %sourceFolder% (und allen Unterordnern) werden in die Endung %targetExtension% umbenannt.
 echo Drücken Sie eine beliebige Taste, um fortzufahren, oder schließen Sie dieses Fenster, um abzubrechen.
 pause >nul
 
-for /r "%folder%" %%g in (*.txt) do ren "%%g" "%%~ng.php"
+pushd "!sourceFolder!"
+
+set "logFile=%sourceFolder%\Umbenennungsprotokoll.txt"
+echo. > "%logFile%"
+
+for /r %%g in (*.%sourceExtension%) do (
+    set "oldName=%%~nxg"
+    set "newName=%%~ng.%targetExtension%"
+    ren "%%g" "!newName!"
+    echo !oldName! wurde in !newName! umbenannt. >> "%logFile%"
+)
+
+popd
 
 echo Umbenennen abgeschlossen! Drücken Sie eine beliebige Taste, um dieses Fenster zu schließen.
 pause >nul
