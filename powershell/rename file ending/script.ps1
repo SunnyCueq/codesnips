2d44a1fb61236567c883dad4c579732b02027e42
SannyQ <91934182+SannyQ@users.noreply.github.com>
2023-06-15 16:26:16 +0200

    Update script.ps1

--- powershell/rename file ending/script.ps1
+++ powershell/rename file ending/script.ps1
@@ -1,1 +1,15 @@
-Get-ChildItem -Recurse -Filter "*.txt" -File | Rename-Item -NewName { $_.Name -replace ".txt", ".php" }
+$sourceFolder = Read-Host "Geben Sie den Pfad zum Verzeichnis ein, in dem sich die Dateien befinden"
+
+$sourceExtension = Read-Host "Geben Sie die Quell-Dateiendung ein, die umbenannt werden soll"
+$targetExtension = Read-Host "Geben Sie die Ziel-Dateiendung ein, in die umbenannt werden soll"
+
+$files = Get-ChildItem -Recurse -Filter "*.$sourceExtension" -File -Path $sourceFolder
+
+foreach ($file in $files) {
+    $newName = $file.Name -replace "$sourceExtension$", "$targetExtension"
+    $newPath = Join-Path -Path $file.DirectoryName -ChildPath $newName
+    Rename-Item -Path $file.FullName -NewName $newName -Force
+    Write-Host "$($file.Name) wurde in $newName umbenannt"
+}
+
+Write-Host "Umbenennen abgeschlossen!"
