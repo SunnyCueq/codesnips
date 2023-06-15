5da1403dc36169f8d3f09fedfff534598e4a4de5
SannyQ <91934182+SannyQ@users.noreply.github.com>
2023-06-15 16:12:32 +0200

    Update glob_include.php

--- php/codes/include_all_files/glob_include.php
+++ php/codes/include_all_files/glob_include.php
@@ -1,5 +1,15 @@
 <?php
-foreach (glob("files/*.php") as $filename)
+$folder = "files"; // Standardordner
+
+// Benutzerprompt für den Ordner
+echo "Geben Sie den Ordnerpfad ein (lassen Sie ihn leer für den Standardordner 'files'): ";
+$userFolder = trim(fgets(STDIN));
+
+if (!empty($userFolder)) {
+    $folder = $userFolder;
+}
+
+foreach (glob("$folder/*.php") as $filename)
 {
     if(is_file($filename) && is_readable($filename)) {
         include $filename;
